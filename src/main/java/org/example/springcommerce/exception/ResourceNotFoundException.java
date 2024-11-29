6aae6b3770dc73f61ab0cb64d18fbf6c33839f8a
52200052_Mai Thị Ánh Như <mainhu3304@gmail.com>
2024-11-29 18:26:06 +0700

    Delete src directory

--- src/main/java/org/example/springcommerce/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package org.example.springcommerce.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/org/example/springcommerce/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package org.example.springcommerce.repository;
-
-import org.example.springcommerce.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
